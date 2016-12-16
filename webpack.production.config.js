09a48932af1031f2052af12d5c3f02d25dd14278
Yifei Zhang <njzhangyifei@gmail.com>
2016-12-15 17:46:29 -0800

    use uglify in production

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -12,6 +12,9 @@ module.exports = {
     devtool: '#source-map',
     plugins: [
         new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: { warnings: false }
+        })
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery",
